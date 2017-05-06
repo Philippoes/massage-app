f5e415addb364927df50632d0968bb223ee2a67c
Philip <Philip.zdb@gmail.com>
2017-05-06 15:31:04 +0200

    Adds attributes + city relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506103042) do
+ActiveRecord::Schema.define(version: 20170506132945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,18 @@ ActiveRecord::Schema.define(version: 20170506103042) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "rating"
+    t.string "address"
+    t.integer "phone"
+    t.string "email"
+    t.integer "pricerange"
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_users_on_city_id"
   end
 
   add_foreign_key "answers", "requests"
   add_foreign_key "answers", "users"
   add_foreign_key "requests", "cities"
+  add_foreign_key "users", "cities"
 end
