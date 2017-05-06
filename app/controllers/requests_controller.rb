71af4db2dd8d60400f9a3a96f0c99a38b26b8957
Philip <Philip.zdb@gmail.com>
2017-05-06 10:29:45 +0200

    Add form and controller action

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,8 +3,20 @@ class RequestsController < ApplicationController
   end
 
   def create
+    request = Request.new(request_params)
+    if request.save
+      flash[:notice] = "Förfrågan skapad"
+      redirect_to(requests_show_path)
+    else
+      flash[:alert] = "Var vänlig fyll i alla fält"
+    end
   end
 
   def show
+  end
+
+  private
+  def request_params
+    params.require(:request).permit(:description, :time, :area)
   end
 end
