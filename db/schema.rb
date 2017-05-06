a8b621f25f92c00db65d858cf3c28753520acd53
Martinlinder <Martin.liiinder@gmail.com>
2017-05-06 14:09:13 +0200

    Fixed city selection

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506083152) do
+ActiveRecord::Schema.define(version: 20170506102929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "requests", force: :cascade do |t|
     t.text "description"
     t.datetime "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_requests_on_city_id"
   end
 
+  add_foreign_key "requests", "cities"
 end
