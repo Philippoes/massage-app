ea2d33dba73ffb2e05a7ac953f731811ed1f2590
felixottander <felix.ottander@yahoo.com>
2017-05-06 15:42:03 +0200

    fixes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,6 @@
 class AnswersController < ApplicationController
 
   def create
-    binding.pry
     @answer = Answer.new(status: true, request: Request.find(params[:id]), user: User.last)
      if @answer.save
       flash[:notice] = "Svar skapat"
